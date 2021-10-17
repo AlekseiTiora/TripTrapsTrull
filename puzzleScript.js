381add863c3b4051ebab577755a0c8a50c9cd6d3
Aleksei Tiora <aleksei.tiora@gmail.com>
2021-10-17 14:23:03 +0300

    Добавьте файлы проекта.

---
+++ puzzleScript.js
@@ -0,0 +1,25 @@
+var lopp=false;
+
+function klik(pilt){
+    pilt.src=kellekord;
+
+    if(vyidukontroll()){
+
+        lopp=true;
+    }
+}
+
+function klikValik(pilt){
+    kellekord=pilt.src;
+    document.getElementById("pilt19").src=kellekord;
+}
+
+//проверяет все картинки и возвращает false если найдёт пустую картинку.
+
+
+// определяем имя файла
+function sisu(nr){
+    var fnimi=document.getElementById("pilt" + nr).src;
+    fnimi=fnimi.split("/").pop();
+    return fnimi;
+}
